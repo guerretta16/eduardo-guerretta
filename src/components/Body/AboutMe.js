e9a8ed4ca8369064fbc06636eae2b00e6fb4dd4e
guerretta16 <guerret98@gmail.com>
2023-01-18 13:18:21 -0600

    about me

--- src/components/Body/AboutMe.js
+++ src/components/Body/AboutMe.js
@@ -26,7 +26,7 @@ const AboutMe = ({isEnglish}) => {
             </h2>
             <div className='aboutMe-grid'>
                 <div className='aboutMe-left'>
-                    <span>{isEnglish?"Hi, i'm Eduardo Guerretta":"Hola, soy Eduardo Guerretta."}</span><br/><br/>I'm 24 years old and i'm an Enginnering of Informatic Systems student. Throughout my studies I've worked on different projects that have allowed me to learn and improve my skills as a web developer<br/><br/>I'm a optimistic, collavorative person, and i'm willing to give my all to improve.
+                    <span>{isEnglish?"Hi, i'm Eduardo Guerretta":"Hola, soy Eduardo Guerretta."}</span><br/><br/>{isEnglish?"I'm 24 years old and i'm an Enginnering of Informatic Systems student. Throughout my studies I've worked on different projects that have allowed me to learn and improve my skills as a web developer":"Tengo 24 años y soy estudiante de Ingeniería en Sistemas Informáticos. A lo largo de mis estudios he trabajado en diferentes proyectos que me han permitido aprender y mejorar mis habilidades como desarrollador web"}<br/><br/>{isEnglish?"I'm a optimistic, collaborative person, and i'm willing to give my all to improve.":"Soy una persona optimista, colaboradora y dispuesta a darlo todo para mejorar."}
                     <br/><br/>    
                     <button className='cvButton' type='button' onClick={dowloadCV}>
                         {isEnglish?"Download CV":"Descargar CV"} 
